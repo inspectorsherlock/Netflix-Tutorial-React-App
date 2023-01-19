e96bbf43e8f1f3521df4d5772994af31134dde0b
Fawwaz <fawwaz.chowdhury@googlemail.com>
2023-01-19 17:42:38 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRGdLSRhLnuuHsB3XhrJVI4fWhzq22O3I",
-  authDomain: "netflix-clone-c642e.firebaseapp.com",
-  projectId: "netflix-clone-c642e",
-  storageBucket: "netflix-clone-c642e.appspot.com",
-  messagingSenderId: "679292406577",
-  appId: "1:679292406577:web:c377badce15b547d13fe10"
+  apiKey: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,
+  authDomain: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,
+  projectId: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,
+  storageBucket: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,
+  messagingSenderId: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,
+  appId: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
